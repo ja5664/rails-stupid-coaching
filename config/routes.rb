eab0f746b0f1ef7872c10c260214a155b6247681
Joe Alexander <ja5664@gmail.com>
2017-11-06 13:40:32 +0000

    Ask coach a question and see his response on the next loaded page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'questions', to: 'pages#questions'
+
+  get 'answers', to: 'pages#answers'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
